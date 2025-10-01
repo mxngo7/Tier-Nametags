7b86f4e05841cac05bf8f0d1c440ce10f61a0fec
mxngo7 <116481095+mxngo7@users.noreply.github.com>
2025-10-01 19:39:39 +1000

    Update mod version

--- src/main/java/me/mxngo/TierNametags.java
+++ src/main/java/me/mxngo/TierNametags.java
@@ -46,7 +46,7 @@ import net.minecraft.util.Pair;
 
 public class TierNametags implements ModInitializer {
 	public static final String MODID = "tiernametags";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.0.1";
 	
 	private static TierNametags instance = new TierNametags();
 	private final Logger logger = LoggerFactory.getLogger(MODID);
